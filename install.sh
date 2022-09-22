aa2346590dee9f4987450e61d1b6ad5ec5778a1d
Bradley Whitcomb <bradwhitcomb@Bradleys-MacBook-Pro.local>
2022-09-22 20:02:54 +0100

    .sh update

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo yum -y install yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+sudo systemctl enable docker.service --now
+sudo systemctl enable containerd.service --now 
+sudo curl -L https://github.com/docker/compose/releases/download/v2.11.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
